783d4d0ef7a25b41405ea14a3329d87822b1019d
bvalmeida <bvalmeida94@gmail.com>
2023-09-07 10:01:10 -0300

    finalizando projeto todo com save em localstorage

--- project-3-todo-list-initial/scripts.js
+++ project-3-todo-list-initial/scripts.js
@@ -11,19 +11,31 @@ const todoList = {
         }
     }, methods: {
         cleanList: function(){
-            this.todoLists = []
+            this.todoLists = [];
+            localStorage.clear();
         },
 
         addTodo: function(){
             if(this.newTodo.message){
-                this.todoLists.push(this.newTodo)
+                this.todoLists.push(this.newTodo);
                 this.newTodo = {
                     active: false
-                }
+                };
+
+                this.storeTodosOnLocalStorage();
             }else{
-                alert('Text not be empyt')
+                alert('Text not be empyt');
             }
         },
+        storeTodosOnLocalStorage: function(){
+            localStorage.setItem('to-dos', JSON.stringify(this.todoLists));
+        }
+    },
+    created(){
+        this.todoLists = localStorage.getItem('to-dos') ? JSON.parse(localStorage.getItem('to-dos')) : this.todoLists;
+    },
+    updated(){
+        this.storeTodosOnLocalStorage();
     }
 };
 
