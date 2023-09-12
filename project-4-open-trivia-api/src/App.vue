265e466dd2f1e3ce12e15f0d252dbebb278701aa
bvalmeida <bvalmeida94@gmail.com>
2023-09-12 19:00:33 -0300

    implementando respostas

--- project-4-open-trivia-api/src/App.vue
+++ project-4-open-trivia-api/src/App.vue
@@ -1,47 +1,97 @@
 <template>
   <div>
     <template v-if="this.question">
+      <p>Pc {{ this.pcPoints }} x Player {{ this.playerPoints }}</p>
+
       <h1 v-html="this.question"></h1>
-  
+
       <template v-for="(answer, index) in this.answers" :key="index">
-        <input type="radio" name="options" value="answer">
-        <label v-html="answer"></label><br>
+        <input 
+        type="radio" 
+        name="options" 
+        :value="answer"
+        v-model="this.chosenAnswer" 
+        />
+        <label 
+        v-html="answer"
+        ></label><br />
       </template>
-      
-  
-      <button class="send" type="button">Send</button>
+
+      <button class="send" 
+      type="button"
+      @click="this.submitAnswer()"
+      >Send</button>
+
+      <label>{{ this.messageCorrectOrNot }}</label>
+
     </template>
   </div>
 </template>
 
 <script>
-const urlApi = 'https://opentdb.com/api.php?amount=1&category=18&difficulty=hard' 
+const urlApi =
+  "https://opentdb.com/api.php?amount=1&category=18&difficulty=hard";
 
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
       question: undefined,
       incorrectAnswers: undefined,
       correctAnswer: undefined,
-    }
+      chosenAnswer: undefined,
+      pcPoints: 0,
+      playerPoints: 0,
+      messageCorrectOrNot: undefined,
+    };
   },
-  computed:{
+  computed: {
     answers() {
       var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
-      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswer
+      );
       return answers;
-    }
+    },
   },
-  created() {
-    this.axios.get(urlApi).then((response) => {
+  methods: {
+    submitAnswer(){
+      this.validateAnswer();
+      this.newQuestion();
+    },
+
+    newQuestion(){
+      this.axios.get(urlApi).then((response) => {
       console.log(response.data.results[0]);
       this.question = response.data.results[0].question;
       this.incorrectAnswers = response.data.results[0].incorrect_answers;
       this.correctAnswer = response.data.results[0].correct_answer;
-    })
-  }
-}
+      });
+    },
+
+    validateAnswer(){
+      if(!this.chosenAnswer){
+        alert('Pick one of the options');
+
+      }else{
+        if(this.chosenAnswer === this.correctAnswer){
+          alert('You got it right');
+          this.playerPoints += 1;
+
+        }else{
+          alert('You got it wrong');
+          this.pcPoints +=1;
+        }
+      }
+    }
+    
+  },  
+  created() {
+    this.newQuestion();
+  },
+};
 
 //https://opentdb.com/api.php?amount=1&category=18&difficulty=hard
 </script>
@@ -56,11 +106,11 @@ export default {
   margin: 60px auto;
   max-width: 960px;
 
-  input[type=radio]{
-    margin: 12px 4px
+  input[type="radio"] {
+    margin: 12px 4px;
   }
 
-  button.send{
+  button.send {
     margin-top: 12px;
     height: 40px;
     min-width: 120px;
